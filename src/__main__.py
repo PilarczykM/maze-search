e34aa80e823d25e71e23ecf529226e52e1977d25
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-01-09 12:59:46 +0100

    Bootstrap application

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,33 @@
-def main() -> None:
+import sys
+from pathlib import Path
+
+from frontier import QueueFrontier, StackFrontier
+from maze import Maze
+
+
+def main(maze_file: Path) -> None:
     """Bootstrap the maze."""
-    print("Maze app")
+    frontier = StackFrontier()
+    frontier = QueueFrontier()
+    maze = Maze(maze_file)
+
+    print("Maze:")
+    maze.print()
+    print("Solving...")
+    maze.solve(frontier)
+    print("States explored: ", maze.num_explored)
+    print("Solution:")
+    maze.print()
+    current_directory = Path(__file__).parent
+    maze.output_image(current_directory / ".." / Path("images/maze.png"), show_explored=True)
 
 
 if __name__ == "__main__":
-    main()
+    num_args = 2
+    if len(sys.argv) != num_args:
+        pass
+        sys.exit("Usage: python maze.py maze.txt")
+
+    path = Path(sys.argv[1])
+    main(path)
+
